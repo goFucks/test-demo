14e13bcb336d58dfd84b03f33a59977ed06fb99a
guodev <875477046@qq.com>
2021-03-30 17:34:54 +0800

    33333master

--- spring-boot-task/src/main/java/com/itstyle/quartz/Application.java
+++ spring-boot-task/src/main/java/com/itstyle/quartz/Application.java
@@ -17,6 +17,6 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		logger.info("爪哇笔记定时任务项目启动2");
-		logger.info("爪哇笔记定时任务项目启动2");
+		logger.info("爪哇笔记定时任务项目启动3333");
 	}
 }
